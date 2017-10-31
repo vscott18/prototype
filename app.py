5d686a72f8ccd9d2eccb47f828931b89ea158339
Victoria Scott <victoriascott2014@gmail.com>
2017-10-31 16:02:04 -0400

    changes

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, redirect, session
+from flask import render_template
+from flask import request
+import sys
+
+#Application initialization
+app = Flask(__name__)
+
+#URL to function mappings
+@app.route('/')
+
+if __name__ == '__main__':
+    app.run()
