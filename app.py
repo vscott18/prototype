801ab20a1bff35d453b4521a7a3cfbdab3f15889
Victoria Scott <victoriascott2014@gmail.com>
2017-11-04 10:31:44 -0400

    changes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, redirect, session
 from flask import render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 #Application initialization
