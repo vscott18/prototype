09ce4bb15c3f2daae9392bf78c03f02e63a5d776
Victoria Scott <victoriascott2014@gmail.com>
2017-11-04 11:36:42 -0400

    adding index

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, redirect, session
 from flask import render_template
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
+from database_functions import *
+from database_test import DatabaseTest
 from flask_migrate import Migrate
+from subprocess import call
 import sys
 
 #Application initialization
 app = Flask(__name__)
 
 #URL to function mappings
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run()
---
+++ database_functions.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+import hashlib
+import datetime
+import random
+from database_declarative import *
+
