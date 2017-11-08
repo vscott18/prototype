5da17ef0556c7940e665ff0dd10c4d3745889ea8
Victoria Scott <victoriascott2014@gmail.com>
2017-11-07 19:20:24 -0500

    changes

---
+++ apicalls.py
@@ -0,0 +1,3 @@
+#API key for open weather: 215314faaa1f9375a6f2a329ce37ef44 
+
+response = requests.get(http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=215314faaa1f9375a6f2a329ce37ef44)
