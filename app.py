dff3c49716228c1d18e92d91ca171e2ed8b4c4a7
Victoria Scott <victoriascott2014@gmail.com>
2017-11-08 00:53:02 -0500

    folder change

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
-
 from flask import Flask, redirect, session
 from flask import render_template
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
-from database_functions import *
-from database_test import DatabaseTest
-from flask_migrate import Migrate
-from subprocess import call
-import sys
+# from flask import request
+# from flask_sqlalchemy import SQLAlchemy
+# # from database_functions import *
+# from flask_migrate import Migrate
+# from subprocess import call
+# import sys
 
-#Application initialization
+# #Application initialization
+# app = Flask(__name__)
+
+# #URL to function mappings
+# @app.route('/')
+# def home_page():
+#     return render_template('index1.html')
+
+# # @app.route('/processquery', methods = ['POST'])
+# # def processquery():
+# #     db = Database()
+# #     db.close()
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug = True)
+
 app = Flask(__name__)
 
-#URL to function mappings
-@app.route('/', methods=['GET', 'POST'])
-def home_page():
+@app.route('/')
+def index():
     return render_template('index1.html')
 
-@app.route('/processquery', methods = ['POST'])
-def processquery():
-    db = Database()
-    db.close()
-    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
