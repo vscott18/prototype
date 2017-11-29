e4cb9b327fb2ce45e3518f17a2ba73e221ac7345
Victoria Scott <victoriascott2014@gmail.com>
2017-11-29 15:15:17 -0500

    wooo changes

--- sportsApi.py
+++ sportsApi.py
@@ -1,18 +1,21 @@
 import base64
 import requests
-
+#
+#getThis = "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/game_boxscore.json?gameid=20171008-SF-IND&teamstats=W,L,T,PF,PA&playerstats=Att,Comp,Yds,TD"
+#r = requests.get(getThis)
+#data = r.json()
 
 def send_request():
     # Request
 
     try:
         response = requests.get(
-            url={pull-url},
+            url= "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/game_boxscore.json?gameid=20171008-SF-IND&teamstats=W,L,T,PF,PA&playerstats=Att,Comp,Yds,TD",
             params={
                 "fordate": "20161121"
             },
             headers={
-                "Authorization": "Basic " + base64.b64encode('{}:{}'.format({vzsn},{benice2me}).encode('utf-8')).decode('ascii')
+                "Authorization": "Basic " + base64.b64encode('{}:{}'.format(vzsn,benice2me).encode('utf-8')).decode('ascii')
             }
         )
         print('Response HTTP Status Code: {status_code}'.format(
@@ -21,3 +24,5 @@ def send_request():
             content=response.content))
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
+
+send_request()
